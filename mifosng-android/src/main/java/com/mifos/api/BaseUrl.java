2e36699e2074752fda5ab6553c59cf800ca8ec86
Indrajeet Kumar <indrajeet.kumar@sap.com>
2021-06-10 11:38:33 +0530

    Updated Branding

--- mifosng-android/src/main/java/com/mifos/api/BaseUrl.java
+++ mifosng-android/src/main/java/com/mifos/api/BaseUrl.java
@@ -11,12 +11,12 @@ package com.mifos.api;
 public class BaseUrl {
 
     public static final String PROTOCOL_HTTPS = "https://";
-    public static final String API_ENDPOINT = "demo.mifos.io";
+    public static final String API_ENDPOINT = "ec2-3-108-41-6.ap-south-1.compute.amazonaws.com";
     public static final String API_PATH = "/fineract-provider/api/v1/";
-    public static final String PORT = "80";
+    public static final String PORT = "8443";
     // "/" in the last of the base url always
 
     public String getName() {
-        return "mifos";
+        return "Yashvi";
     }
 }
